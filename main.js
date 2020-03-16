a4390f87ecd23dfe1f467141dc2ed22a073d7da4
FadyBengoufa <fady.bengoufa@gmail.com>
2020-03-16 17:07:54 +0100

    trying to fix the display table

--- main.js
+++ main.js
@@ -12,17 +12,31 @@ function displayTurkishLCs(){
         type: 'GET',
         success: function(data){
             mainData = data.analytics.total_applications.buckets.buckets;
-
+            var  arrayTurkishLCs = [];
             for (let i=0; i<mainData.length; i++){
-                $('#table thead tr').append("" +
-                    "<th>"+ mainData[i].key + "</th>"
-                );
+                
+                arrayTurkishLCs.push(mainData[i].key);
             }
-
+            /**
+             * Changing the sort method so it can arrange the values from small ID to Bigger ID
+             */
+            function compare(a,b){
+               return a-b;
+            }
+            arrayTurkishLCs.sort(compare);
+            //console.log(arrayTurkishLCs);
+            /*
+            arrayTurkishLCs.forEach(turkishLCKey => {
+                $('#table thead tr').append("" +
+                    "<th>"+ turkishLCKey + "</th>"
+            );
+            
+            });
+            */
+            
         }
     })
 }
-
 // This function will display the IDs of the algerian LCs
 function displayAlgerianLCs(){
     $.ajax({
@@ -34,7 +48,7 @@ function displayAlgerianLCs(){
             for (let i=0; i<mainData.length; i++){
                 $('#table tbody').append("" +
                     "<tr>" +
-                    "<td>"+mainData[i].key +"</td>" +
+                    "<th scope='row'>"+mainData[i].key +"</th>" +
                     "</tr>"+
                     "");
             }
@@ -45,6 +59,7 @@ function displayAlgerianLCs(){
 
 
 
+var arrayDataE2E = [];
 
 function displayApplications(){
     $.ajax({
@@ -52,13 +67,18 @@ function displayApplications(){
         dataType: 'json',
         type: 'GET',
         success: function (data) {
-            turkLCs = data.analytics.total_applications.buckets.buckets;
             algeriaLcs = data.analytics.total_applications.children.buckets;
+            arrayDataE2E = [];
             for (let j=0; j<algeriaLcs.length; j++){
-                for (let i=0; i<turkLCs.length; i++){
-                    if(algeriaLcs[j].children.buckets[i].key === turkLCs[i].key){
-                        console.log("Benak"+ algeriaLcs[j].children.buckets[i].key + "Turk : "+turkLCs[i].key);
-                    }
+                //console.log('Benak: '+algeriaLcs[j].key);
+                
+                for (let i=0; i<algeriaLcs[j].children.buckets.length; i++){
+                    //console.log('Istanbul: '+algeriaLcs[j].children.buckets[i].key);
+                    //console.log("Benak : "+ algeriaLcs[j].key + " Turk : "+algeriaLcs[j].children.buckets[i].key);
+                    arrayDataE2E.push({
+                        x:algeriaLcs[j].key,
+                        y:algeriaLcs[j].children.buckets[i].key
+                    });
                 }
             }
         }
@@ -71,14 +91,70 @@ function test(){
         dataType: 'json',
         type: 'GET',
         success: function(data){
-            console.log(data.analytics);
-            $('#table tbody').append("<tr><td>"+data.analytics.total_applications.doc_count+ "</td><tr>");
+            
+            /*function compare(a,b){
+                return ((a.y-b.y));
+            }
+            
+            arrayDataE2E.sort(compare);
+            */
+            //console.log(arrayDataE2E);
+
+        /**
+         * Separate the data of each LC of Algeria with Turkey's LCs 
+         * to properly manipulate it in the table and Display it.
+         */
+            var arrayOfEachLC = [];
+            var arrayLCObject = [];
+            var j = i = 0;
+           while(i < arrayDataE2E.length){
+                while(arrayDataE2E[i].x == arrayDataE2E[j].x){
+                    arrayOfEachLC.push({
+                        x:arrayDataE2E[i].x,
+                        y:arrayDataE2E[j].y
+                    });
+                    j++;
+                }
+                //arrayLCObject.push(arrayOfEachLC);
+                i = j;
+                //Empty the array each time the ID of LC algeria is different
+                arrayOfEachLC = [];
+              
+            }
+            
+            //console.log(arrayLCObject);
         }
     })
 }
+
 displayTurkishLCs();
+
 displayAlgerianLCs();
-test();
+
+
 displayApplications();
 
+test();
 
+
+
+/*
+301 {
+    813 => number applications 113
+    528
+    1893
+    ...
+    2849
+}
+302 {
+    528
+    1893
+    ...
+    813 number applications 100
+    2849
+}
+
+301 , 302 {
+    813 => 113+100
+}
+*/
